61ad3f2ce9e306c8f47f10b7ece30a1608441b6c
jihwan <dhwlghksy@gmail.com>
2023-12-29 16:14:15 +0900

    Baekjoon 1373

--- Algorithm.py
+++ Algorithm.py
@@ -1,9 +1,10 @@
-while True:
-    a = input()
-    if a == '0':
-        break
-    b = a[::-1]
-    if(a == b):
-        print('yes')
-    else:
-        print('no')
+a = input("")
+while len(a) % 3 != 0:
+    a = '0' + a
+
+res = ''
+for i in range(0, len(a), 3):
+    temp = int(a[i])*4 + int(a[i+1]) * 2 + int(a[i+2]) 
+    res = res + str(temp)
+    
+print(res)
