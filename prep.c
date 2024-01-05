973d05bfc4148265cd695ed5598185bd9149d52a
jihwan <dhwlghksy@gmail.com>
2024-01-05 20:34:14 +0900

    Baekjoon 2775

--- prep.c
+++ prep.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 
 
-int findRoom(int h, int w, int n){
-    int x,y;
-    y = n % h;
-    x = n / h + 1;
-    if(y == 0){
-        y = h;
-        x--;
+int resNum(int k,int n){
+    int num[15][15] = {0};
+    for(int i = 0; i <= k; i++){
+        for(int j = 1; j <= n; j++){
+            if(i == 0){
+                num[i][j] = j;
+            }else{
+                int cnt = 0;
+                for(int t = 1; t <= j; t++){
+                    cnt += num[i-1][t];
+                }
+                num[i][j] = cnt;
+            }
+        }
     }
-    return 100*y + x;
+    return num[k][n];
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-        int h, w, nth;
-        scanf("%d %d %d", &h, &w, &nth);
-        printf("%d\n", findRoom(h, w, nth));
+    int T;
+    scanf("%d", &T);
+    for(int i = 0; i < T; i++){
+        int k, n;
+        scanf("%d", &k);
+        scanf("%d", &n);
+        printf("%d\n", resNum(k,n));
     }    
 
     return 0;
-}
+} 
