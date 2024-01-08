4ce776023eb4d582de23ed63e166b9b869ad41ec
jihwan <dhwlghksy@gmail.com>
2024-01-09 02:34:00 +0900

    Baekjoon 3003

--- prep.c
+++ prep.c
@@ -1,33 +1,9 @@
 #include <stdio.h>
 
 
-int resNum(int k,int n){
-    int num[15][15] = {0};
-    for(int i = 0; i <= k; i++){
-        for(int j = 1; j <= n; j++){
-            if(i == 0){
-                num[i][j] = j;
-            }else{
-                int cnt = 0;
-                for(int t = 1; t <= j; t++){
-                    cnt += num[i-1][t];
-                }
-                num[i][j] = cnt;
-            }
-        }
-    }
-    return num[k][n];
-}
-
 int main() {
-    int T;
-    scanf("%d", &T);
-    for(int i = 0; i < T; i++){
-        int k, n;
-        scanf("%d", &k);
-        scanf("%d", &n);
-        printf("%d\n", resNum(k,n));
-    }    
-
+    int k, q, l, b, kn, p;
+    scanf("%d %d %d %d %d %d", &k, &q, &l, &b, &kn, &p);
+    printf("%d %d %d %d %d %d", 1-k, 1-q, 2-l, 2-b, 2-kn, 8-p);
     return 0;
 } 
