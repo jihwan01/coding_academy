ac436af33e401859912d46dabe6a393fbef51f05
jihwan <dhwlghksy@gmail.com>
2024-01-09 20:37:28 +0900

    Baekjoon 1012

--- Algorithm.py
+++ Algorithm.py
@@ -1,36 +1,38 @@
-n = input()
+T = int(input())
 
-def sum(n): # O(N^2)
-    res = 0
-    for i in range(1,n+1):
-        temp = i
-        cnt = 0;
-        while temp <= n:
-            if(temp == n):
-                res += 1
-                break
-            cnt += 1
-            temp += (i + cnt)
-    return res
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
+def BFS(x,y):
+  q = [(x,y)]
+  matrix[x][y] = 0
+  
+  while q:
+    x,y = q.pop(0)
+    
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-def sum2(n): # O(N)
-    count = 0
-    start = 1
-    end = 1
-    sum = 1
+      if(nx >= 0 and nx < m and ny >= 0 and ny < n and matrix[nx][ny] == 1):
+        q.append((nx, ny))
+        matrix[nx][ny] = 0 
+  
+  
 
-    while start <= n//2:
-        if sum < n:
-            end += 1
-            sum += end
-        elif sum > n:
-            sum -= start
-            start += 1
-        else:
-            count += 1
-            sum -= start
-            start += 1
-
-    return count + 1 
-print(sum2(int(n)))
+for i in range(T):
+  m, n, k = map(int, input().split())
+  
+  matrix = [[0 for _ in range(n)] for _ in range(m)]
+  res = 0
+  
+  for j in range(k):
+    x,y = map(int, input().split())
+    matrix[x][y] = 1
+  
+  for a in range(m):
+    for b in range(n):
+      if(matrix[a][b] == 1):
+        BFS(a,b)
+        res += 1
+  print(res)
