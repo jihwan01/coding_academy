1d1cc961c7b424fa4a5bf3155026ced4e77471e1
JH <jh@JHui-MacBookPro.local>
2024-01-17 18:40:14 +0900

    Baekjoon 1913

--- prep.c
+++ prep.c
@@ -1,24 +1,57 @@
 #include <stdio.h>
 
+int main(){
+    int N;
+    long long d;
+    scanf("%d", &N);
+    scanf("%lld", &d);
+	long long res[1000][1000] = {0,};
 
-int main() {
-    char a[1005],b[1005];
-    scanf("%s\n", a);
-    scanf("%s", b);
-    int i = 0;
-    int len_a = 0;
-    int len_b = 0;
-    while(a[i++] != '\0'){
-        len_a++;
+    int R = N / 2;
+    int nx = N / 2;
+    int ny = N / 2;
+    long long cnt = 1;
+    int rx, ry;
+    
+    res[nx][ny] = cnt;
+    if(cnt == d){
+        rx = nx+1;
+        ry = ny+1;
     }
-    i = 0;
-    while(b[i++] != '\0'){
-        len_b++;
+    cnt++;
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0}; 
+
+    for(int r = 1; r <= R; r++){
+        nx -= 1;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 2*r; j++){
+                if(i != 0 || j != 0){
+                    nx += dx[i];
+                    ny += dy[i];
+                }
+                if(cnt == d){
+                    rx = nx+1;
+                    ry = ny+1;
+                }
+                res[nx][ny] = cnt;
+                cnt++;
+            }   
+        } 
     }
-    if(len_a >= len_b){
-        printf("go");
-    }else{
-        printf("no");
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(j == N-1){
+                printf("%lld\n", res[i][j]);
+            }else{
+                printf("%lld ", res[i][j]);
+            }
+
+        }
     }
-    return 0;
-} 
+
+    printf("%d %d\n", rx, ry);
+    
+	return 0;
+}
