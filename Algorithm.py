ceb76db91d69da6952b298543ab9cca2d2e58f9d
JH <dhwlghksy@gmail.com>
2024-01-20 04:54:46 +0900

    Baekjoon 17413

--- Algorithm.py
+++ Algorithm.py
@@ -2,22 +2,38 @@ import sys
 
 sys.setrecursionlimit(100000000)
 input = sys.stdin.readline
+
+# 17413
 s = input().strip()
-bs = input().strip()
-
-blen = len(bs)
-index = 0
-st = []
-for c in s:
-    st.append(c)
-    if(st[-blen:] == list(bs)):
-        for _ in range(blen):
-            st.pop()
 
 
-if(len(st) == 0):
-    print('FRULA') 
-else:
-    for i in st:
-        print(i, end ="")
-    print()
+def reverse_words_in_string(s):
+    tag = False
+    word_stack = []
+    result = ''
+
+    for char in s:
+        if char == '<':
+            while word_stack:
+                result += word_stack.pop()
+            tag = True
+            result += char
+        elif char == '>':
+            tag = False
+            result += char
+        elif tag:
+            result += char
+        else:
+            if char == ' ':
+                while word_stack:
+                    result += word_stack.pop()
+                result += char
+            else:
+                word_stack.append(char)
+
+    while word_stack:
+        result += word_stack.pop()
+
+    return result
+
+print(reverse_words_in_string(s))
