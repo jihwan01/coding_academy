7205af229d45c2b428f6ea15ddff42866264c98a
JH <jh@JHui-MacBookPro.local>
2024-01-20 03:25:45 +0900

    Baekjoon 1240

--- Algorithm.py
+++ Algorithm.py
@@ -1,50 +1,33 @@
 import sys
-sys.setrecursionlimit(100000000)
+
 input = sys.stdin.readline
 
-N, Q = map(int, input().split())
+n,m = map(int, input().split())
+tree = [[] for _ in range(n+1)]
 
-matrix = list(map(int, input().split()))
-tree = [0 for _ in range(N*4+5)]
+for _ in range(n-1):
+    a, b, l = map(int, input().split())
+    tree[a].append((b, l))
+    tree[b].append((a, l))
     
-def init(s, e, index):
-    if s == e:
-        tree[index] = matrix[s]
-        return matrix[s]
-    m = (s+e) // 2
-    tree[index] = init(s, m, index*2) + init(m+1, e, index*2 + 1)
+def bfs(start, find):
+    q = []
+    q.append((start, 0))
+    visited = [False for _ in range(n + 1)]
+    visited[start] = True
+    while q:
+        a, d = q.pop(0)
+        visited[a] = True
+        if(a == find):
+            return d
+        
+        for i, id in tree[a]:
+            if(not visited[i]):
+                q.append((i, id+d))
+                
+for _ in range(m):
+    s, f = map(int, input().split())
+    print(bfs(s,f))
     
-    return tree[index]
 
-def intervalSum(s, e, index, l, r):
-    if e < l or s > r:
-        return 0
-    if s >= l and e <= r:
-        return tree[index]
-    m = (s+e) // 2
-    return intervalSum(s, m, index*2, l, r) + intervalSum(m+1, e, index*2+1, l, r)
-
-def update(s, e, index, x, v):
-    if s > x or x > e:
-        return
-    m = (s+e) // 2
-    tree[index] += v
-    if s == e:
-        return
-    update(s, m, index*2, x, v)
-    update(m+1, e, index*2+1, x, v)
-    
-    
-init(0, N-1, 1)
-
-results = []
-for i in range(Q):
-    x,y,a,b = map(int, input().split())
-    if(y < x):
-        x, y = y, x
-    results.append(intervalSum(0,N-1,1,x-1,y-1))
-    dist = b - matrix[a-1]
-    matrix[a-1] = b
-    update(0, N-1, 1, a-1, dist)
-
-sys.stdout.write('\n'.join(map(str,results)))
+                
--- prep.c
+++ prep.c
@@ -1,57 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int N;
-    long long d;
-    scanf("%d", &N);
-    scanf("%lld", &d);
-	long long res[1000][1000] = {0,};
-
-    int R = N / 2;
-    int nx = N / 2;
-    int ny = N / 2;
-    long long cnt = 1;
-    int rx, ry;
-    
-    res[nx][ny] = cnt;
-    if(cnt == d){
-        rx = nx+1;
-        ry = ny+1;
+    int days[13] = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    char* day_s[8] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+    // 1 : Mon , 2: Tue, 3 : Weds, 4: Thurs, 5: Fri, 6 : Sat, 7 : Sun
+    int day = 1;
+    int x, y;
+    scanf("%d %d", &x, &y);
+    for(int i = 1; i < x; i++){
+        day += days[i] % 7;
     }
-    cnt++;
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0}; 
-
-    for(int r = 1; r <= R; r++){
-        nx -= 1;
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 2*r; j++){
-                if(i != 0 || j != 0){
-                    nx += dx[i];
-                    ny += dy[i];
-                }
-                if(cnt == d){
-                    rx = nx+1;
-                    ry = ny+1;
-                }
-                res[nx][ny] = cnt;
-                cnt++;
-            }   
-        } 
+    day += y % 7 - 1;
+    if(day > 7){
+        day -= 7;
     }
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            if(j == N-1){
-                printf("%lld\n", res[i][j]);
-            }else{
-                printf("%lld ", res[i][j]);
-            }
-
-        }
+    if(day == 0){
+        day = 7;
     }
-
-    printf("%d %d\n", rx, ry);
-    
+    printf("%s", day_s[day]);
 	return 0;
-}
+}   
