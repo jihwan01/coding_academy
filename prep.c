56997bcabc9b577656b8e8181c8de4f7b830f783
JH <dhwlghksy@gmail.com>
2024-01-23 02:25:04 +0900

    Sort(Bubble, Insertion, Selection)

--- prep.c
+++ prep.c
@@ -1,22 +1,60 @@
+// Bubble Sort
 #include <stdio.h>
 
+void bubble(int* arr, int N){
+    for(int i=N; i>1; i--){
+		for(int j=1; j<i; j++){
+			if(arr[j] > arr[j+1]){
+				int tmp = arr[j];   
+				arr[j] = arr[j+1]; 
+				arr[j+1] = tmp;
+			}
+		}
+	}
+}
+
+void selection(int* arr, int N){
+    for(int i = 1; i < N; i++){
+        int min = i;
+        for(int j = i+1; j <= N; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+void insertion(int* arr, int N){
+    for(int i = 2; i <= N; i++){
+        int key = arr[i];
+        int index = i;
+        for(int j = i-1; j >= 1; j--){
+            if(key < arr[j]){
+                arr[j+1] = arr[j];
+                index = j;
+            }else{
+                break;
+            }
+        }
+        arr[index] = key;
+    }
+}
+
 int main(){
-    int days[13] = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    char* day_s[8] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-    // 1 : Mon , 2: Tue, 3 : Weds, 4: Thurs, 5: Fri, 6 : Sat, 7 : Sun
-    int day = 1;
-    int x, y;
-    scanf("%d %d", &x, &y);
-    for(int i = 1; i < x; i++){
-        day += days[i] % 7;
+    int N;
+	scanf("%d", &N);
+	int A[10000] = {0,};
+    for(int i=1; i<=N; i++){
+		scanf("%d", &A[i]);
+	}
+    // bubble(A, N);
+    // selection(A,N);
+    insertion(A,N);
+    for(int i = 1; i <= N; i++){
+        printf("%d\n", A[i]);
     }
-    day += y % 7 - 1;
-    if(day > 7){
-        day -= 7;
-    }
-    if(day == 0){
-        day = 7;
-    }
-    printf("%s", day_s[day]);
 	return 0;
-}   
+}
