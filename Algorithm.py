f38faf519d5b725fe704376554d686280e8248c0
JH <dhwlghksy@gmail.com>
2024-01-27 15:42:14 +0900

    Baekjoon 1302

--- Algorithm.py
+++ Algorithm.py
@@ -1,8 +1,28 @@
-#2941
-
-s = input()
-arr = ['dz=', 'c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
-for w in arr:
-    s = s.replace(w, '+')
+#1302
+class Book:
+    def __init__(self, name):
+        self.name = name
+        self.count = 1
     
-print(len(s))
+    def sell(self):
+        self.count += 1
+    
+def getBestSeller(books):
+    books.sort(key = lambda x : (-x.count, x.name))
+    return books[0]
+    
+N = int(input())
+books = []
+for _ in range(N):
+    title = input()
+    find = False
+    for book in books:
+        if book.name == title:
+            book.sell()
+            find = True
+            break
+    if not find:
+        books.append(Book(title))
+
+print(getBestSeller(books).name)
+
