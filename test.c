5db2051199435abbc9d77b9565c6c7fa2d7756f4
JH <dhwlghksy@gmail.com>
2024-02-07 21:30:57 +0900

    Baekjoon 1009

--- test.c
+++ test.c
@@ -1,40 +1,22 @@
 #include <stdio.h>
 
-//5622
-int main() {
-    char st[16];
-    scanf("%s", st);
-    int res = 0;
-    char* stp = st;
-    // using ascii code
-    // ABC : 65-67, DEF : 68-70, GHI: 71-73 JKL : 74-76, MNO : 77-79, PQRS: 80-83, TUV : 84-86, WXYZ: 87-90
-    while(*stp != '\0'){
-        if(*stp < 68){
-            res += 3;
-            stp++;
-        }else if(*stp < 71){
-            res += 4;
-            stp++;   
-        }else if(*stp < 74){
-            res += 5;
-            stp++;   
-        }else if(*stp < 77){
-            res += 6;
-            stp++;   
-        }else if(*stp < 80){
-            res += 7;
-            stp++;   
-        }else if(*stp < 84){
-            res += 8;
-            stp++;   
-        }else if(*stp < 87){
-            res += 9;
-            stp++;   
-        }else{
-            res += 10;
-            stp++;   
-        }
+//1009
+int lastComNum(int a, int b){
+    int res = a % 10;
+    for(int i = 0; i < b-1; i++){
+        res = res * a % 10;
     }
-    printf("%d", res);
+    return res;
+}
+int main() {
+    int N;
+    scanf("%d", &N);
+    int a,b;
+    int res;
+    for(int i = 0 ; i < N; i++){
+        scanf("%d %d", &a, &b);
+        res = lastComNum(a,b);
+        printf("%d\n",res == 0 ? 10 : res);
+    }
     return 0;
 }
