71ac75e76f7f5e6a7b5fcc76b3f52f319a2d49c5
JH <dhwlghksy@gmail.com>
2024-02-28 14:54:34 +0900

    Baekjoon 1712

--- prep.c
+++ prep.c
@@ -1,60 +1,15 @@
-// Bubble Sort
+//1712
 #include <stdio.h>
 
-void bubble(int* arr, int N){
-    for(int i=N; i>1; i--){
-		for(int j=1; j<i; j++){
-			if(arr[j] > arr[j+1]){
-				int tmp = arr[j];   
-				arr[j] = arr[j+1]; 
-				arr[j+1] = tmp;
-			}
-		}
-	}
-}
-
-void selection(int* arr, int N){
-    for(int i = 1; i < N; i++){
-        int min = i;
-        for(int j = i+1; j <= N; j++){
-            if(arr[j] < arr[min]){
-                min = j;
-            }
-        }
-        int temp = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp;
+int main() {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    if(B >= C){
+        printf("-1\n");
+        return 0;
+    }else{
+        int result = A / (C-B) + 1;
+        printf("%d\n", result);
+        return 0;
     }
-}
-
-void insertion(int* arr, int N){
-    for(int i = 2; i <= N; i++){
-        int key = arr[i];
-        int index = i;
-        for(int j = i-1; j >= 1; j--){
-            if(key < arr[j]){
-                arr[j+1] = arr[j];
-                index = j;
-            }else{
-                break;
-            }
-        }
-        arr[index] = key;
-    }
-}
-
-int main(){
-    int N;
-	scanf("%d", &N);
-	int A[10000] = {0,};
-    for(int i=1; i<=N; i++){
-		scanf("%d", &A[i]);
-	}
-    // bubble(A, N);
-    // selection(A,N);
-    insertion(A,N);
-    for(int i = 1; i <= N; i++){
-        printf("%d\n", A[i]);
-    }
-	return 0;
 }
