17067bc1c178c569e724b435a6ae42f8008242d9
JH <dhwlghksy@gmail.com>
2024-02-28 15:46:01 +0900

    Baekjoon 2309

--- prep.c
+++ prep.c
@@ -1,42 +1,46 @@
-//9093
+//2309
 
 #include <stdio.h>
 
-void printReverse(char st[]){
-    char word[21];
-    int i = 0;
-    int total_i = 0;
-    while(st[total_i] != '\0'){
-        if(st[total_i] == ' '){
-            for(int j = i-1; j >= 0; j--){
-                printf("%c", word[j]);
-            }
-            printf(" ");
-            i = 0;
-        }else {
-            if(st[total_i] != '\n'){
-                word[i] = st[total_i];
-                i++;
-            }
-        }
-        total_i++;
-    }
-    if(i > 0){
-        for(int j = i-1; j >= 0; j--){
-            printf("%c", word[j]);
-        }
-        printf("\n");
-    }
-}
 
 int main() {
-    int n;
-    scanf("%d\n", &n);
-    char sen[1002];
-    for(int i = 0; i < n; i++){
-        fgets(sen, 1001, stdin);
-        printReverse(sen);
+    int h[9];
+    int cand[7];
+    for(int i = 0; i < 9; i++){
+        scanf("%d", &h[i]);
     }
+    int sum = 0;
+    int idx = 0;
+    for(int i = 0; i < 9; i++){
+        for(int j = i+1; j < 9; j++){
+            for(int k = 0; k < 9; k++){
+                if(k != i && k != j){
+                    sum += h[k];
+                    cand[idx++] = h[k];
+                }
+            }
+            if(sum == 100){
+                int min = 200;
+                int minIdx = 0;
+                for(int i = 0; i < 7; i++){
+                    for(int j = 0; j < 7; j++){
+                        if(cand[j] < min){
+                            min = cand[j];
+                            minIdx = j;
+                        }
+                    }
+                    printf("%d\n", cand[minIdx]);
+                    cand[minIdx] = 1000;
+                    min = 200;
+                }
+                return 0;
+            }else{
+                idx = 0;
+                sum = 0;
+            }
+        }
+    }
+    
 
     return 0;
 }
