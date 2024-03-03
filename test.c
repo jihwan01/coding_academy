eee424a8bd4fea917d1a7ec11eff3a21a8fdea60
JH <dhwlghksy@gmail.com>
2024-03-03 22:34:44 +0900

    Baekjoon 10811

--- test.c
+++ test.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 
-// 10870
+// 10811
 
 int main() {
-    int n;
-
-    scanf("%d", &n);
-    int fibo[25];
-
-    for(int i = 0; i <= n; i++){
-        if(i == 0 || i == 1){
-            fibo[i] = i;
-        }else{
-            fibo[i] = fibo[i-1] + fibo[i-2];
+    int n, m;
+    int num[101];
+    int temp[101];
+    int a,b;
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n+1; i++){
+        num[i] = i;
+    }
+    for(int i = 0; i<m; i++){
+        scanf("%d %d", &a, &b);
+        for(int j = 0; j < b - a + 1; j++){
+            temp[j] = num[a+j];
+        }
+        for(int j = 0; j < b - a + 1; j++){
+            num[a+j] = temp[b-a-j];
         }
     }
-    
-    printf("%d", fibo[n]);
+
+    for(int i = 1; i <= n; i++){
+        if(i != n){
+            printf("%d ", num[i]);
+        }else{
+            printf("%d\n", num[i]);
+        }
+    }
 
     return 0;
  }
