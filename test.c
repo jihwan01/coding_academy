3c576207aa395f4bcf683e98d92ba68772b7318f
JH <dhwlghksy@gmail.com>
2024-03-03 22:23:00 +0900

    Baekjoon 10870

--- test.c
+++ test.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
-// 2798
+// 10870
 
 int main() {
-    int N, M;
-    int num[100];
+    int n;
 
-    scanf("%d %d", &N, &M);
-    for(int i = 0; i < N; i++){
-        scanf("%d", &num[i]);
-    }
-    int max = 0;
-    int sum;
-    for(int i = 0; i < N; i++){
-        for(int j = i+1; j < N; j++){
-            for(int k = j+1; k < N; k++){
-                sum = num[i] + num[j] + num[k];
-                if(sum <= M && sum > max){
-                    max = sum;
-                }
-            }
+    scanf("%d", &n);
+    int fibo[25];
+
+    for(int i = 0; i <= n; i++){
+        if(i == 0 || i == 1){
+            fibo[i] = i;
+        }else{
+            fibo[i] = fibo[i-1] + fibo[i-2];
         }
     }
-
-    printf("%d", max);
+    
+    printf("%d", fibo[n]);
 
     return 0;
  }
